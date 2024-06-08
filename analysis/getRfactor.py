34db1c4ea0f3f377cf9a2d27bff68a35f165f283
Pam Osborn Popp <pamosbornpopp@gmail.com>
2024-06-08 18:10:25 -0500

    since rasterio cannot be used with pyodide yet, just calculate all the rfactors ahead of time for each tennessee zip code

--- analysis/getRfactor.py
+++ analysis/getRfactor.py
@@ -6,30 +6,15 @@ import os
 # to make sure we import the files from the correct directory
 dir_path  = os.path.dirname(os.path.realpath(__file__))
 
-
-# get zipcode from user
-zipcode = 37212
-
+# zipcode dataframe
 tnzips = pd.read_csv(dir_path + '/tn_zipcodes.csv')
 
-# check if zipcode is valid
-if not (zipcode in tnzips['zip'].unique()):
-    print("Error: this zipcode is not found in our database! Is it a Tennessee zip code?")
-else:
-    print("Calculating...")
-    # Get coordinates from zipcode from file (coords in decimal degrees)
-    def get_coordinates(zipcode):
-        lat = tnzips.loc[tnzips['zip']==zipcode,'latitude'].values[0]
-        long = tnzips.loc[tnzips['zip']==zipcode,'longitude'].values[0]
-        return lat,long
-
-    # Example usage
-    # address = input(f'your address')
-    lat,long = get_coordinates(zipcode)
-
+def getRfac(zipcode):
+    lat = tnzips.loc[tnzips['zip']==zipcode,'latitude'].values[0]
+    long = tnzips.loc[tnzips['zip']==zipcode,'longitude'].values[0]
     # Open your raster file
     # dataset = rasterio.open('./tn_rfactors.tif')
-    with rasterio.open(dir_path + '/tn_rfactors.tif') as dataset:
+    with rasterio.open(dir_path + '/Rfactors.tif') as dataset:
         # convert decimal degrees (global coordinates) into dataset's CRS
         # Define the target CRS. the CRS of the raster dataset
         target_crs = dataset.crs
@@ -40,8 +25,10 @@ else:
         # Sample the pixel value at the transformed coordinates
         for val in dataset.sample([(x[0], y[0])], indexes=1):
             rval = val[0]
+    return rval
 
+# this line takes about 5 seconds to run 
+tnzips['rfactor'] = tnzips.apply(lambda row: getRfac(row['zip']), axis=1)
 
-    # Print the value
-    print("The R Factor at the given zipcode is:", rval)
-
+# write dataframe with rfactor column to file
+tnzips.to_csv(dir_path + '/tn_zips_rvals.csv')
