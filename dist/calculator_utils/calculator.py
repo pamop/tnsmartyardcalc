72bbc4f8cc1634631b57eba43683c40e759460c3
Pam Osborn Popp <pamosbornpopp@gmail.com>
2024-06-10 10:02:28 -0500

    update calc utils again due to bug

--- dist/calculator_utils/calculator.py
+++ dist/calculator_utils/calculator.py
@@ -26,9 +26,11 @@ dir_path = '/home/pyodide/calculator_utils' # calculator_utils is a zip unpacked
 zipdf = pd.read_csv(dir_path + '/tn_zips_rvals.csv') # zip codes and corresponding counties and lat/long coords
 lsdf = pd.read_csv(dir_path + '/ls_values.csv') # ls values given slope percent and length
 
-# some helper functions for calculating the RUSLE value
 def find_nearest_value(input_value, available_values):
   return min(available_values, key=lambda x: abs(x - input_value))
+
+# print(np.asarray(list(table.keys()), dtype=float))
+
 def find_intersecting_length(slope_percentage, slope_length):
   # Find the nearest slope percentage in the table
   nearest_slope_percentage = find_nearest_value(slope_percentage, lsdf['slope_percents'].unique())
@@ -37,6 +39,16 @@ def find_intersecting_length(slope_percentage, slope_length):
   # Find the intersecting length (LS) from the table
   intersecting_length = lsdf.loc[(lsdf['slope_lengths']==nearest_slope_length) & (lsdf['slope_percents']==nearest_slope_percentage), 'ls'].values[0]
   return intersecting_length
+
+# area = float(input("Enter the area of the land in square feet: "))
+# slope_percentage = float(
+#     input('what is your slope percentage in a number\
+# (ie: 40% = 40):'))
+# slope_length = float(input('whats your slope length in feet:'))
+# percentNative = (float(
+#     input('Enter your percent of your yard that is native plants \
+# as a number (ie: 40% = 40): '))) / 100
+
 
 percentNormal = 1 - percentNative
 
@@ -49,13 +61,11 @@ else:
 
 LS = find_intersecting_length(slope_percentage, slope_length)
 K = 0.295
+R = 190 # TODO: Get r factor from getRfactor.py
 cFactorNative = 0.05
 cFactorGrass = 0.1
 
-# Lastly we need the r factor, which we get from Rfactor.py based on their zip code
-R = zipdf.loc[zipdf['zip']==zipcode,'rfactor'].values[0] # e.g. 200
 
-# method to compute erosion
 def erosion(after):
   if after:
      return (((R * K * LS * cFactorNative * P)*percentNative) +\
@@ -67,7 +77,6 @@ afterVol = (erosion(True)*2204)/90
 beforeVol = (erosion(False)*2204)/90
 afterLB = erosion(True)*2204
 beforeLB = erosion(False)*2204
-
 erosion_response = f'Your erosion is {afterLB:.2f} pounds per year or {afterVol:.2f} cubic feet per year, but if you did not have native plants, ' + \
       f'your erosion would be {beforeLB:.2f} pounds per year or {beforeVol:.2f} cubic feet per year.'
 print(f'Your erosion is {afterLB:.2f} pounds per year or {afterVol:.2f} cubic feet per year, but if you did not have native plants, '\
